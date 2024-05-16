cd6a15aac12d81067dcb83f29f9e12e6139aea34
asmit basnet <basnetasmit123@gmail.com>
2024-05-15 21:53:40 -0400

    v1

---
+++ app.js
@@ -0,0 +1,31 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+document.getElementById('appointment-form').addEventListener('submit', function(event) {
+	event.preventDefault();
+	var successMessage = document.getElementById('success-message');
+	successMessage.textContent = 'Form submitted successfully!';
+	this.reset();
+  });
